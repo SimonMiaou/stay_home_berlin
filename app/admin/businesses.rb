dabe87666497a18c442662c41dd955c76551725f
Simon Lacroix <simon@miaou.be>
2020-03-28 18:29:16 +0100

    Allow update of delivery areas

--- app/admin/businesses.rb
+++ app/admin/businesses.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Business do
-  permit_params :name, :address, :business_type, :business_sub_type, :description, :comment
+  permit_params :name,
+                :address,
+                :business_type,
+                :business_sub_type,
+                :description,
+                :comment,
+                business_delivery_areas_attributes: [:delivery_area_id]
 
   index do
     selectable_column
@@ -12,5 +18,14 @@ ActiveAdmin.register Business do
     column :created_at
     column :updated_at
     actions
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.has_many :business_delivery_areas, new_record: true, allow_destroy: true do |da_f|
+      da_f.input :delivery_area
+    end
+    f.actions
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -4,5 +4,7 @@ class Business < ApplicationRecord
   validates :name, presence: true
 
   has_many :business_delivery_areas, dependent: :destroy
-  has_many :delivery_area, through: :business_delivery_areas
+  has_many :delivery_areas, through: :business_delivery_areas
+
+  accepts_nested_attributes_for :business_delivery_areas, allow_destroy: true
 end
