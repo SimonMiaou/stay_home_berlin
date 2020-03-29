919481add525d46d3fa0883c21234fe199d3697c
Simon Lacroix <simon@miaou.be>
2020-03-29 21:35:27 +0200

    Show opening hours

--- app/models/business.rb
+++ app/models/business.rb
@@ -8,4 +8,13 @@ class Business < ApplicationRecord
   has_many :opening_hours, dependent: :destroy
 
   accepts_nested_attributes_for :opening_hours, allow_destroy: true
+
+  def opening_hours_to_show
+    opening_hours.group_by(&:day_as_string).map do |day_as_string, opening_hours|
+      opening_hours.map! do |opening_hour|
+        opening_hour.opening_time.strftime('%H:%M') + ' - ' + opening_hour.closing_time.strftime('%H:%M')
+      end
+      [day_as_string, opening_hours.to_sentence]
+    end
+  end
 end
