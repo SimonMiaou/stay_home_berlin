09d882c886071a4cc3b9a3d6f327be310cd6cfb3
Simon Lacroix <simon@miaou.be>
2020-03-30 21:20:45 +0200

    Show the page :bowtie:

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,6 @@ class Page < ApplicationRecord
   validates :slug, presence: true, uniqueness: true
   validates :menu_order, numericality: { only_integer: true }, allow_nil: true
   validates :title, presence: true
+
+  scope :for_menu, -> { where.not(menu_order: nil).order(:menu_order) }
 end
